e06998ad1d1cf2fcc0f73d167348153654673f94
nathan_lu <nathan_lu@paradise-soft.com.tw>
2019-06-12 12:08:46 +0800

    add node sh

--- init-db/init-node.sh
+++ init-db/init-node.sh
@@ -2,16 +2,7 @@
 
 set -e
 
-sleep 30
-until mysql -u root -p${MYSQL_ROOT_PASSWORD} -h master; do 
-    >&2 echo "MySQL master is unavailable -sleeping"
-    sleep 5
-done
-
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -h localhost \
--e "CREATE USER '${MYSQL_REPLICATION_USER}'@'${MYSQL_HOST}' IDENTIFIED BY '${MYSQL_REPLICATION_USER_PWD}'; \
-GRANT REPLICATION SLAVE ON *.* TO '${MYSQL_REPLICATION_USER}'@'${MYSQL_HOST}';"
-
+sleep 60
 
 mysql -u root -p${MYSQL_ROOT_PASSWORD} -h localhost \
 -e "CHANGE MASTER TO MASTER_HOST='master', \
